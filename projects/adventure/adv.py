24e8b7db0cf5bf2e243545f47354d0a2253f8310
Anna Morris <annapatton444@gmail.com>
2020-10-25 15:00:04 -0500

    1000 moves

--- projects/adventure/adv.py
+++ projects/adventure/adv.py
@@ -31,31 +31,43 @@ traversal_path = []
 
 # directions and opposites "bread_crumbs" for back-tracking
 directions = {"n": "s", "e": "w", "s": "n", "w": "e"}
-bread_crumbs = []
-visited = {}
 
-while len(visited) < len(room_graph)-1:
-    if player.current_room.id not in visited:
-        visited[player.current_room.id]=player.current_room.get_exits()
-        print(bread_crumbs)
-        if len(bread_crumbs)>0:
-            #removing exits we just came from so we don't visit them again
-            step_back = bread_crumbs[-1]
-            visited[player.current_room.id].remove(step_back)
+##first pass
+# bread_crumbs = []
+# visited = {}
+# while len(visited) < len(room_graph)-1:
+#     if player.current_room.id not in visited:
+#         visited[player.current_room.id]=player.current_room.get_exits()
+#         if len(bread_crumbs)>0:
+#             #removing exits we just came from so we don't visit them again
+#             step_back = bread_crumbs[-1]
+#             visited[player.current_room.id].remove(step_back)
        
-    while len(visited[player.current_room.id]) < 1:
-        prev = bread_crumbs.pop()
-        traversal_path.append(prev)
-        player.travel(prev)
-    current = visited[player.current_room.id].pop(0)
-    traversal_path.append(current)
-    bread_crumbs.append(directions[current])
-    player.travel(current)
+#     while len(visited[player.current_room.id]) < 1:
+#         prev = bread_crumbs.pop()
+#         traversal_path.append(prev)
+#         player.travel(prev)
+#     current = visited[player.current_room.id].pop(0)
+#     traversal_path.append(current)
+#     bread_crumbs.append(directions[current])
+#     player.travel(current)
 
-
-
-print("visited",visited)
-
+##recursive
+def recurse(current_room, visited = set()):
+    bread_crumbs=[]
+    for direction in player.current_room.get_exits():
+        player.travel(direction)
+        if player.current_room in visited:
+            player.travel(directions[direction])
+        else:
+            visited.add(player.current_room)
+            bread_crumbs.append(direction)
+            bread_crumbs = bread_crumbs + \
+                recurse(player.current_room, visited)
+            player.travel(directions[direction])
+            bread_crumbs.append(directions[direction])
+    return bread_crumbs
+traversal_path = recurse(player.current_room)
 
 
 
